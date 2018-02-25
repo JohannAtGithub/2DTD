b0034ca89106cc0dfc81058ac3cd15403fbea915
JohannAtGithub <johann.finder@t-online.de>
2018-02-25 13:31:07 +0100

    create a helpers class

--- 2DTD/src/data/Boot.java
+++ 2DTD/src/data/Boot.java
@@ -4,16 +4,37 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import static helpers.Artist.*;
+
 public class Boot {
 	
 	public Boot() {
-		Display.setTitle("2DTD");
-		try {
-			Display.setDisplayMode(new DisplayMode(600, 400));
-			Display.create();
-		} catch (LWJGLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		BeginSession();
+		
+		float width = 50, height = 50, x = 100, y = 100;
+		
+		/*
+		 * game loop
+		 * don't end until Display is not closed
+		 */
+		while(!Display.isCloseRequested()) {
+			glBegin(GL_LINES);
+			glVertex2f(10, 10);
+			glVertex2f(100, 100);
+			glEnd();
+			
+			glBegin(GL_QUADS);
+			glVertex2f(x, y);
+			glVertex2f(x + width, y);
+			glVertex2f(x + width, y + height);
+			glVertex2f(x, y + height);
+			glEnd();
+			
+			Display.update();
+			Display.sync(60);
 		}
 	}
 	
