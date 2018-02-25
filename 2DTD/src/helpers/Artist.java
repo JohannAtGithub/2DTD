8976682765a8dcf090f22af8636ae5a813e0af8e
JohannAtGithub <johann.finder@t-online.de>
2018-02-25 13:47:50 +0100

    create method to draw the tile textures

--- 2DTD/src/helpers/Artist.java
+++ 2DTD/src/helpers/Artist.java
@@ -1,22 +1,15 @@
 package helpers;
 
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glOrtho;
-import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.opengl.Texture;
 
 public class Artist {
 
-	public static final int WIDTH = 600, HEIGHT = 400;
+	public static final int WIDTH = 1280, HEIGHT = 960;
 	
 	public static void BeginSession() {
 		Display.setTitle("2DTD");
@@ -30,8 +23,9 @@ public class Artist {
 		
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(0, 600, 400, 0, 1, -1); //setting up the camera
+		glOrtho(0, WIDTH, HEIGHT, 0, 1, -1); //setting up the camera
 		glMatrixMode(GL_MODELVIEW);
+		glEnable(GL_TEXTURE_2D);
 	}
 	
 	public static void DrawQuad(float x, float y, float width, float height) {
@@ -40,6 +34,22 @@ public class Artist {
 		glVertex2f(x + width, y);
 		glVertex2f(x + width, y + height);
 		glVertex2f(x, y + height);
+		glEnd();
+	}
+	
+	public static void DrawQuadTex(Texture tex, float x, float y, float width, float height) {
+		tex.bind();
+		glTranslatef(x, y, 0);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(0, 0);
+		glTexCoord2f(1, 0);
+		glVertex2f(width, 0);
+		glTexCoord2f(1, 1);
+		glVertex2f(width, height);
+		glTexCoord2f(0, 1);
+		glVertex2f(0, height);
+		glLoadIdentity();
 		glEnd();
 	}
 }
