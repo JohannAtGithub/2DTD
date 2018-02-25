8726b3b2b45f73e7053a4a7c6354ab22b360fa4d
JohannAtGithub <johann.finder@t-online.de>
2018-02-25 14:08:43 +0100

    create enum with different tile types and load the texture

--- 2DTD/src/data/Boot.java
+++ 2DTD/src/data/Boot.java
@@ -15,17 +15,17 @@ public class Boot {
 		
 		BeginSession();
 		
-		 Texture t = QuickLoad("dirt64");
-		 Texture t2 = QuickLoad("grass64");
 		
+		Tile tile = new Tile(0, 0, 64, 64, TileType.Grass);
+		Tile tile2 = new Tile(64, 0, 64, 64, TileType.Dirt);
 		/*
 		 * game loop
 		 * don't end until Display is closed
 		 */
 		while(!Display.isCloseRequested()) {
 			
-			DrawQuadTex(t, 0, 0, 64, 64);
-			DrawQuadTex(t2, 64, 0, 64, 64);
+			DrawQuadTex(tile.getTexture(), tile.getX(), tile.getY(), tile.getWidth(), tile.getHeight());
+			DrawQuadTex(tile2.getTexture(), tile2.getX(), tile2.getY(), tile2.getWidth(), tile2.getHeight());
 			
 			Display.update();
 			Display.sync(60);
